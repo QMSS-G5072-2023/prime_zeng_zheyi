d01b4e41912a4f89fa1b831db719c73025ea1282
zzheyi <zz3155@columbia.edu>
2023-10-29 18:52:22 -0400

    test functions

--- prime_zz3155/tests/test_prime_zz3155.py
+++ prime_zz3155/tests/test_prime_zz3155.py
@@ -1,1 +1,9 @@
-from prime_zz3155 import prime_zz3155
+from src.prime_zz3155.is_prime import is_prime
+
+def test_is_prime():
+    assert is_prime(2) == True
+    assert is_prime(3) == True
+    assert is_prime(4) == False
+    assert is_prime(5) == True
+    assert is_prime(17) == True
+    assert is_prime(18) == False
